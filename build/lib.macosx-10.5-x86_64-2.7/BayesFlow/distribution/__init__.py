ee5fc3dafaf8fc563d5689f0231e37ff84bcee8d
Jonas Wallin <jonas.wallin81@gmail.com>
2015-05-17 00:37:04 +0200

    removed things

--- build/lib.macosx-10.5-x86_64-2.7/BayesFlow/distribution/__init__.py
+++
@@ -1,3 +0,0 @@
-from distribution_cython import multivariatenormal, invWishart, Wishart  # @UnresolvedImport
-from priors import normal_p_wishart , Wishart_p_nu
-__all__ = ['multivariatenormal', 'normal_p_wishart','invWishart','Wishart','Wishart_p_nu']
--- build/lib.macosx-10.5-x86_64-2.7/bayesianmixture/PurePython/distribution/__init__.py
+++
@@ -1,4 +0,0 @@
-from multivariatenormal import multivariatenormal
-from wishart import invWishart, Wishart
-from priors import nu_class
-__all__ = ['multivariatenormal','invWishart','nu_class','Wishart']
