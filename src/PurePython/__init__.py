7a9947b5d5872b2f340dce4d86178b2cda9cdd3f
kjohnsson <kerstin.johnsson@hotmail.com>
2015-05-29 14:55:45 +0200

    Remove import of features in dev version

--- src/PurePython/__init__.py
+++ src/PurePython/__init__.py
@@ -6,5 +6,5 @@ Created on Wed Jun 18 15:51:02 2014
 """
 
 from GMM import mixture
-from GMMrep import mixture_repeat_measurements
-__all__ = ['mixture','mixture_repeat_measurements']
+#from GMMrep import mixture_repeat_measurements
+__all__ = ['mixture']#,'mixture_repeat_measurements']
