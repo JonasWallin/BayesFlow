bb64a7216828a49979aff500eca8e8a6c645b0c1
Jonas Wallin <jonas.wallin81@gmail.com>
2015-08-17 01:01:35 +0200

    found an annoying print

--- src/hier_GMM_MPI.py
+++ src/hier_GMM_MPI.py
@@ -408,11 +408,9 @@ class hierarical_mixture_mpi(object):
 
 		self.comm.Bcast([cl_off, MPI.INT])
 		
-		print('before cl_off in {rank}'.format(rank = rank))
 		if cl_off[0]:
 			warnings.warn('One cluster turned off in all samples')
 			
-		print('after cl_off in {rank}'.format(rank = rank))
 
 	def set_simulation_param(self,sim_par):
 		self.set_p_labelswitch(sim_par['p_sw'])
